813c33a742ac576f8a5d95bc91d21f8b1521d044
Thomas Tortorini <thomastortorini@gmail.com>
2018-12-15 01:01:00 +0100

    .headLink.selected

--- js/mainLayer.js
+++ js/mainLayer.js
@@ -2,36 +2,13 @@
 
 const mainLayer = {
 	init() {
-		const pages = document.querySelectorAll( ".page" ),
-			nameToObj = new Map( [
-				[ "", { obj: homePage || {}, elem: DOM.homePage } ],
-				[ "u", { obj: userPage, elem: DOM.userPage } ],
-			] );
-
+		this._nameToObj = new Map( [
+			[ "",  { elem: DOM.homePage, headElem: DOM.headIcon, obj: homePage || {} } ],
+			[ "u", { elem: DOM.userPage, headElem: DOM.headUser, obj: userPage } ],
+		] );
 		DOM.headLogout.onclick = this._logoutBtnClick.bind( this );
-		DOM.pages.append.apply( DOM.pages, pages );
-		router.on( [], ( [ path0 ], [ prevPath0 ] ) => {
-			errorPage.hide();
-			if ( path0 !== prevPath0 ) {
-				const prev = nameToObj.get( prevPath0 ),
-					page = nameToObj.get( path0 );
-
-				if ( prev ) {
-					prev.elem.classList.remove( "show" );
-					if ( prev.obj.hide ) {
-						prev.obj.hide();
-					}
-				}
-				if ( !page ) {
-					errorPage.show( 404 );
-				} else {
-					page.elem.classList.add( "show" );
-					if ( page.obj.show ) {
-						page.obj.show();
-					}
-				}
-			}
-		} );
+		DOM.pages.append.apply( DOM.pages, document.querySelectorAll( ".page" ) );
+		router.on( [], ( a, b ) => this._routerFn( a[ 0 ], b[ 0 ] ) );
 	},
 	updateHead( u ) {
 		DOM.headUser.href = `#/u/${ u.username }`;
@@ -42,6 +19,30 @@ const mainLayer = {
 	},
 
 	// private:
+	_routerFn( path0, prevPath0 ) {
+		errorPage.hide();
+		if ( path0 !== prevPath0 ) {
+			const prev = this._nameToObj.get( prevPath0 ),
+				page = this._nameToObj.get( path0 );
+
+			if ( prev ) {
+				prev.elem.classList.remove( "show" );
+				prev.headElem.classList.remove( "selected" );
+				if ( prev.obj.hide ) {
+					prev.obj.hide();
+				}
+			}
+			if ( !page ) {
+				errorPage.show( 404 );
+			} else {
+				page.elem.classList.add( "show" );
+				page.headElem.classList.add( "selected" );
+				if ( page.obj.show ) {
+					page.obj.show();
+				}
+			}
+		}
+	},
 	_logoutBtnClick() {
 		apiClient.logout().then( res => {
 			console.log( "logout", res );
