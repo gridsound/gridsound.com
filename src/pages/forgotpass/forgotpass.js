091224425bd5370e5634a10f0a26a06c51975d89
mr21 <thomastortorini@gmail.com>
2025-12-03 21:58:55 +0100

    pages/forgotpass: handle error msgs directly here

--- src/pages/forgotpass/forgotpass.js
+++ src/pages/forgotpass/forgotpass.js
@@ -20,12 +20,16 @@ class forgotpassPage {
 		GSUdomAddClass( btn, "btn-loading" );
 		error.textContent = "";
 		gsapiClient.$recoverPassword( email.value )
-			.finally( () => GSUdomRmClass( btn, "btn-loading" ) )
 			.then( () => {
 				GSUdomAddClass( DOM.forgotpassPage, "sended" );
-			}, res => {
-				error.textContent = res.msg;
-			} );
+			}, err => {
+				error.textContent =
+					err.code === 400 ? "This is not a valid email" :
+					err.code === 404 ? "The email is not in the database" :
+					err.code === 409 ? "A recovering email has already been sent to this address less than 1 day ago" :
+					`Error ${ err.code }`
+			} )
+			.finally( () => GSUdomRmClass( btn, "btn-loading" ) );
 		return false;
 	}
 }
